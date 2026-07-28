c141aea69038c07e5b6780f62ec3d9d9395ffe99
Dimon17ndex <dimay17ndex2009@yandex.ru>
2026-07-28 18:46:14 +0300

    Update login.ts

--- api/login.ts
+++ api/login.ts
@@ -37,3 +37,21 @@ export default async function handler(req: any, res: any) {
     return res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 }
+
+// Обработчик запроса проверки пароля
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
+
+    const { password } = req.body || {};
+    
+    // Введенный пользователем пароль сравниваем со скрытым значением
+    const SECRET_PASSWORD = process.env.CONSOLE_PASS || "console_dima_razrab";
+
+    if (password === SECRET_PASSWORD) {
+        return res.status(200).json({ success: true });
+    } else {
+        return res.status(401).json({ success: false, message: 'Неверный пароль' });
+    }
+}
